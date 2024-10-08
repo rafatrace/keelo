4bc3992554ce0cefbd3fbc6550541aac3cc3459f
Rafael Morais <rafaelcanical@gmail.com>
2024-10-08 21:53:15 +0200

    Added global types declarations to correct place

--- vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-declare const __API_KEY__: string
-declare const __AUTH_DOMAIN__: string
-declare const __PROJECT_ID__: string
-declare const __STORAGE_BUCKET__: string
-declare const __MESSAGING_SENDER_ID__: string
-declare const __APP_ID__: string
-declare const __MEASUREMENT_ID__: string
