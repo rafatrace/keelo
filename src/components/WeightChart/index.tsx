a9d00f44d27f817d8e5df2afdbd75af279bca3b0
Rafael Morais <rafaelcanical@gmail.com>
2024-10-12 16:47:21 +0200

    Make it responsive

--- src/components/WeightChart/index.tsx
+++ src/components/WeightChart/index.tsx
@@ -1,4 +1,4 @@
-import { Area, AreaChart, CartesianGrid, Tooltip, YAxis } from 'recharts'
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, YAxis } from 'recharts'
 import styles from './styles.module.css'
 import CustomTooltip from './CustomTooltip'
 
@@ -30,27 +30,36 @@ const WeightChart = ({ data }: TWeightChartProps) => {
 
   return (
     <div className={styles.chartContainer}>
-      <AreaChart width={495} height={300} data={data}>
-        <defs>
-          <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#3880f6" stopOpacity={0.5} />
-            <stop offset="95%" stopColor="#3880f6" stopOpacity={0.05} />
-          </linearGradient>
-        </defs>
-        <CartesianGrid strokeDasharray="3 3" stroke="var(--n40)" />
-        <YAxis
-          ticks={ticks.slice()}
-          axisLine={false}
-          tickLine={false}
-          fontSize={12}
-          stroke="var(--n60)"
-          domain={domain}
-          width={30}
-          tickMargin={6}
-        />
-        <Tooltip content={({ active, payload }) => <CustomTooltip active={active} payload={payload} />} />
-        <Area type="monotone" dataKey="Weight" stroke="#3880f6" strokeWidth={2} fillOpacity={1} fill="url(#gradient)" />
-      </AreaChart>
+      <ResponsiveContainer width="100%" height={300}>
+        <AreaChart data={data}>
+          <defs>
+            <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#3880f6" stopOpacity={0.5} />
+              <stop offset="95%" stopColor="#3880f6" stopOpacity={0.05} />
+            </linearGradient>
+          </defs>
+          <CartesianGrid strokeDasharray="3 3" stroke="var(--n40)" />
+          <YAxis
+            ticks={ticks.slice()}
+            axisLine={false}
+            tickLine={false}
+            fontSize={12}
+            stroke="var(--n60)"
+            domain={domain}
+            width={30}
+            tickMargin={6}
+          />
+          <Tooltip content={({ active, payload }) => <CustomTooltip active={active} payload={payload} />} />
+          <Area
+            type="monotone"
+            dataKey="Weight"
+            stroke="#3880f6"
+            strokeWidth={2}
+            fillOpacity={1}
+            fill="url(#gradient)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
   )
 }
