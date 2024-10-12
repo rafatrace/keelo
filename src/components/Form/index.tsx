7ca9506a2b2ff5e1f90a41c2d19c251ea3a97956
Rafael Morais <rafaelcanical@gmail.com>
2024-10-12 15:15:49 +0200

    Handle input value to avoid string characters

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,6 +1,6 @@
 import { useAuth } from '@/providers/AuthProvider'
 import styles from './styles.module.css'
-import { FormEvent, useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import { addWeight } from '@/queries/weights'
 
 type TFormProps = {
@@ -19,15 +19,28 @@ const Form = ({ fetchAllWeights }: TFormProps) => {
    */
   const saveNewWeight = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+
     await addWeight(weight, currentUser.uid)
     fetchAllWeights()
     setWeight('')
   }
 
+  /**
+   * Handle change
+   */
+  const setValue = (e: ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.value
+    const regex = /^[\d,]*\.?\d*$/
+
+    if (regex.test(val)) {
+      setWeight(val)
+    }
+  }
+
   return (
     <div className={styles.formContainer}>
       <form onSubmit={saveNewWeight} className={styles.form}>
-        <input type="text" value={weight} onChange={(e) => setWeight(e.target.value)} placeholder="Ready to weight?" />
+        <input type="text" value={weight} onChange={setValue} placeholder="Ready to weight?" />
         <button>Save</button>
       </form>
     </div>
